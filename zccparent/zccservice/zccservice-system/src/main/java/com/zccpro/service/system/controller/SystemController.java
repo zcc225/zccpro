62322802f4b9eb8fe249d00245f45fc849db313d
zhangchangchao <zhangchangchao@qianmi.com>
2020-11-30 13:01:13 +0800

    [fix][注释swagger2 相关内容  ]

--- zccparent/zccservice/zccservice-system/src/main/java/com/zccpro/service/system/controller/SystemController.java
+++ zccparent/zccservice/zccservice-system/src/main/java/com/zccpro/service/system/controller/SystemController.java
@@ -1,8 +1,8 @@
 package com.zccpro.service.system.controller;
 
 import com.zccpro.service.system.service.ISystemService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+//import io.swagger.annotations.Api;
+//import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "system")
 @AllArgsConstructor
-@Api(tags = "zccpro-system")
+//@Api(tags = "zccpro-system")
 public class SystemController {
 
 
@@ -21,14 +21,14 @@ public class SystemController {
     private final ISystemService systemService;
 
     @GetMapping(value = "list")
-    @ApiOperation(value = "system list接口")
+//    @ApiOperation(value = "system list接口")
     public Object list() {
         return systemService.list();
     }
 
 
     @GetMapping(value = "page")
-    @ApiOperation(value = "system page接口")
+//    @ApiOperation(value = "system page接口")
     public Object page() {
         return systemService.page();
     }
